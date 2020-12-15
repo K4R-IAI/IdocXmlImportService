2207f15e435c0fd75e74e5a10d2165fe6844844b
Gautam <gautam.dange2007@gmail.com>
2020-12-15 14:58:31 +0100

    entry change

--- test2.sh
+++ test2.sh
@@ -1,4 +1,4 @@
-curl --location --request GET 'http://localhost:3000/xmlIDOC' \
+curl --location --request GET 'http://localhost:3000/getLastXMLEntry' \
 --header 'Content-Type: application/json' \
 --header 'Accept: application/json' \
 --header 'X-Requested-With: X' \
